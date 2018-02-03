e4c3021403915aed930ca32263ed9a91b006dbda
Vipul Wairagade <sonu.wairagade@gmail.com>
2018-02-03 19:48:28 +0530

    Create submit.php

---
+++ submit.php
@@ -0,0 +1,17 @@
+<?php
+// Add name tags to input in html: name,email,phone,submit
+
+if ( isset( $_POST['submit'] ) ) {
+    $con = mysqli_connect("localhost:3306", "conso", "Conso123@", "conso");
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $phone=$_POST['phone'];
+
+    $query = mysqli_query($con, "INSERT into ceo (NAME,EMAIL,CONTACT) values('$name','$email','$phone')");
+     mysqli_close($con);
+}
+?>
