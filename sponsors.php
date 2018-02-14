98224e90f4b1aee02ae04db4bee0438831fb9984
Priyank Nagarnaik <priyank.nagarnaik@gmail.com>
2018-02-14 14:07:32 +0000

    added new sponsors | new sponsor page design

---
+++ sponsors.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+    <!-- Begin Head -->
+    <!-- Begin Head -->
+    <head>
+        <!-- Basic -->
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Consortium'18 | E-CELL, VNIT</title>
+        <meta name="keywords" content="E-cell VNIT entreprenuship cell consortium 18" />
+        <meta name="description" content="Consortium'18 E-CELL VNIT annual entrepreneurship summit">
+        <meta name="author" content="E-cell">
+
+        <!-- Web Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i|Montserrat:400,700" rel="stylesheet">
+
+        <!-- Vendor Styles -->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/animate.css" rel="stylesheet" type="text/css"/>
+        <link href="vendor/themify/themify.css" rel="stylesheet" type="text/css"/>
+        <link href="vendor/scrollbar/scrollbar.min.css" rel="stylesheet" type="text/css"/>
+        <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css"/>
+        <link href="vendor/swiper/swiper.min.css" rel="stylesheet" type="text/css"/>
+
+        <!-- Theme Styles -->
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <link href="css/materialize.css" rel="stylesheet" type="text/css"/>
+        <link href="css/global/global.css" rel="stylesheet" type="text/css"/>
+
+        <!-- Theme Skins -->
+        <link href="css/theme/red.css" rel="stylesheet" type="text/css"/>
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+        <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+
+
+
+    </head>
+    <!-- End Head -->
+
+    <!-- Body -->
+    <body>
+
+        <!--========== HEADER ==========-->
+        <?php include("header.php")?>
+        <!--========== END HEADER ==========-->
+
+        <!--========== PROMO BLOCK ==========-->
+        <div class="g-bg-position--center js__parallax-window" style="background: black 50% 0 no-repeat fixed;">
+            <div class="g-container--md g-text-center--xs g-padding-y-80--xs">
+                <h1 class="g-font-size-40--xs g-font-size-50--sm g-font-size-60--md g-color--white"  style="text-align:center; g-letter-spacing--1">Sponsors</h1>
+            </div>
+        </div>
+        <!--========== END PROMO BLOCK ==========-->
+
+        <!--========== PAGE CONTENT ==========-->
+
+<!-- sponsers-->
+					<article id="spons" class="container" style="background: #ddf 50% 0 no-repeat fixed;">
+						<div class="page-wrapper">
+
+							<div class="content-padding clearfix">
+							  <div class="row"  style="text-align:center;padding:20px;">
+							    <h2>Our Whatever Sponsors</h2>
+							  </div>
+							<div class="row">
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/aiesec.png">
+							</div>
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/brainweave.png">
+							</div>
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/chaturideas.png">
+							</div>
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/civn.png">
+							</div>
+							</div>
+							<div class="row">
+
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/desi.png">
+							</div>
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/DTDC.png">
+							</div>
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/E-Cafe.png">
+							</div>
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/e-cellbits.png">
+							</div>
+							</div>
+							<div class="row">
+
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/ecelliitb.png">
+							</div>
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/ecellnitk.png">
+							</div>
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/ecellnitraipur.png">
+							</div>
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/ecelluttrakhand.png">
+							</div>
+							</div>
+							<div class="row">
+
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/festPav.png">
+							</div>
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/festsinfo.png">
+							</div>
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/iB Hubs.png">
+							</div>
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/kiitecell.png">
+							</div>
+							</div>
+							<div class="row">
+
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/knowafest.png">
+							</div>
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/makeinindia.png">
+							</div>
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/nitconclave.png">
+							</div>
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/nitrourkela.png">
+							</div>
+							</div>
+							<div class="row">
+
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/nitsilchar.png">
+							</div>
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/nittrichy.png">
+							</div>
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/PM Yuva Yojna.png">
+							</div>
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/rainbowgreeners.png">
+							</div>
+							</div>
+							<div class="row">
+
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/startupindia.png">
+							</div>
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/startupstories.png">
+							</div>
+							<div class="col m3 s6">
+							  <img src="/img/SponS/temp/swachindia.png">
+							</div>
+							<div class="col m3 s6">
+							<img src="/img/SponS/temp/TiE.png">
+							</div>
+							</div>
+							<div class="row">
+							<div class="col-md-3">
+							  <img src="/img/SponS/temp/veracity.png">
+							</div>
+							<div class="col-md-3">
+							  <img src="/img/SponS/temp/youpreneur.png">
+							</div>
+							</div>
+
+							</div>
+						</div>
+					</article>
+        <!--========== END PAGE CONTENT ==========-->
+
+        <!--========== FOOTER ==========-->
+        <?php include("footer.php");?>
+        <!--========== END FOOTER ==========-->
+
+        <!-- Back To Top -->
+        <a href="javascript:void(0);" class="s-back-to-top js__back-to-top"></a>
+
+        <!--========== JAVASCRIPTS (Load javascripts at bottom, this will reduce page load time) ==========-->
+        <!-- Vendor -->
+        <script type="text/javascript" src="vendor/jquery.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.migrate.min.js"></script>
+        <script type="text/javascript" src="vendor/bootstrap/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.smooth-scroll.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.back-to-top.min.js"></script>
+        <script type="text/javascript" src="vendor/scrollbar/jquery.scrollbar.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.parallax.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.wow.min.js"></script>
+
+        <!-- General Components and Settings -->
+        <script type="text/javascript" src="js/global.min.js"></script>
+        <script type="text/javascript" src="js/components/header-sticky.min.js"></script>
+        <script type="text/javascript" src="js/components/scrollbar.min.js"></script>
+        <script type="text/javascript" src="js/components/wow.min.js"></script>
+        <!--========== END JAVASCRIPTS ==========-->
+
+    </body>
+    <!-- End Body -->
+</html>
