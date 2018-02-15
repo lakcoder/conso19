79aff663d9ada3b51f75b8ca7a5a246459834bd3
Vipul Wairagade <sonu.wairagade@gmail.com>
2018-02-15 21:40:54 +0530

    Add files via upload

---
+++ expo.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+    <!-- Begin Head -->
+    <head>
+	    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113907534-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-113907534-1');
+</script>
+
+        <!-- Basic -->
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Startup Expo | E-CELL, VNIT</title>
+        <meta name="keywords" content="E-cell VNIT entreprenuship cell consortium 18 startup expo ecell vnit consortium startup expo vnit " />
+        <meta name="description" content="Consortium'18 E-CELL VNIT Annual Entrepreneurship Summit">
+        <meta name="author" content="E-cell">
+
+        <!-- Web Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i|Montserrat:400,700" rel="stylesheet">
+
+        <!-- Vendor Styles -->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/animate.css" rel="stylesheet" type="text/css"/>
+        <link href="vendor/themify/themify.css" rel="stylesheet" type="text/css"/>
+        <link href="vendor/scrollbar/scrollbar.min.css" rel="stylesheet" type="text/css"/>
+        <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet" type="text/css"/>
+        <link href="vendor/swiper/swiper.min.css" rel="stylesheet" type="text/css"/>
+
+        <!-- Theme Styles -->
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
+        <link href="css/materialize.css" rel="stylesheet" type="text/css"/>
+        <link href="css/global/global.css" rel="stylesheet" type="text/css"/>
+
+        <!-- Theme Skins -->
+        <link href="css/theme/red.css" rel="stylesheet" type="text/css"/>
+
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="img/icon.png" type="image/x-icon">
+        <link rel="apple-touch-icon" href="img/icon.png">
+    </head>
+    <!-- End Head -->
+
+    <!-- Body -->
+    <body>
+
+        <!--========== HEADER ==========-->
+        <?php include("header.php")?>
+        <!--========== END HEADER ==========-->
+
+        <!--========== PROMO BLOCK ==========-->
+        <div class="s-promo-block-v3 g-bg-position--center g-fullheight--sm" style="background: url('img/1920x1080/swades-landing.jpg');">
+            <div class="container g-ver-center--sm g-padding-y-125--xs g-padding-y-0--sm">
+                <div class="g-margin-t-30--xs g-margin-t-0--sm g-margin-b-30--xs g-margin-b-70--md">
+                    <h1 class="g-font-size-35--xs g-font-size-45--sm g-font-size-50--lg g-color--white">Startup Expo</h1><h2 class="g-color--white-opacity-light">Grass root Innovations beyond Imagination</h2>
+                </div>
+                <div class="row">
+                    <div class="col-sm-8 col-sm-push-4 g-margin-b-50--xs g-margin-b-0--md">
+                        <div class="s-promo-block-v3__divider g-display-none--xs g-display-block--md"></div>
+                        <div class="row">
+                            <div class="col-sm-6 g-margin-b-30--xs g-margin-b-0--md">
+                                <div class="wow fadeInLeft" data-wow-duration=".3" data-wow-delay=".4s">
+                                    <p class="g-font-size-18--xs g-color--white">Get an insight into blurred segments of the village life and pave them towards a clear path that focuses on a pragmatic, yet a sustainable solution.</p>
+                                </div>
+                            </div>
+                            <!--<div class="col-sm-5 col-sm-offset-1">
+                                <div class="clearfix">
+                                    <div class="pull-left">
+                                        <div class="wow fadeInLeft" data-wow-duration=".3" data-wow-delay=".3s">
+                                            <span class="s-promo-block-v3__date g-font-size-100--xs g-font-size-135--lg g-font-weight--300 g-color--primary">1<span class="g-font-size-20--xs g-font-size-30--lg">Lac</span></span>
+                                        </div>
+                                    </div>
+                                    <div class="wow fadeInLeft" data-wow-duration=".3" data-wow-delay=".1s">
+                                        <span class="s-promo-block-v3__month g-font-size-18--xs g-font-size-22--lg g-font-weight--300 g-color--white">Prize Money</span>
+                                        <span class="s-promo-block-v3__year g-font-size-18--xs g-font-size-22--lg g-font-weight--300 g-color--white">And more!</span>
+                                    </div>
+                                </div>
+                            </div>-->
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-sm-pull-8">
+                        <div class="wow fadeInLeft" data-wow-duration=".3" data-wow-delay=".5s">
+                            <a href="#newform" title="Register">
+                                <i class="s-icon s-icon--lg s-icon--white-bg g-radius--circle ti-arrow-down"></i>
+                                <span class="text-uppercase g-font-size-13--xs g-color--white g-padding-x-15--xs">Register Now!</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--========== END PROMO BLOCK ==========-->
+
+        <!--========== PAGE CONTENT ==========-->
+        <!-- Features -->
+        <div class="container g-padding-x-40--sm g-padding-x-20--xs g-padding-y-20--xs g-padding-y-50--sm">
+            <h1>About</h1>
+            <div class="card" id="event-card-bg">
+    		    <div class="card-tabs">
+    		      <ul class="tabs tabs-fixed-width">
+    		        <li class="tab"><a class="active" href="#bizmantra4">DETAILS</a></li>
+    		        <li class="tab"><a href="#bizmantra5">OPPORTUNITIES</a></li>
+    		      </ul>
+    		    </div>
+    		    <div class="card-content code">
+    		    	<div id="bizmantra4">
+                        <h6><b>Date :- 9th March, 2018 – 11th March, 2018 </b></h6>
+                        <h6><b>Venue :- Visvesvaraya National Institute of Technology, Nagpur</b></h6>
+    		    	    <br>
+    		    	    <h5><b>Cost :-</b> Rs 1,000</h5>
+                        <h5><b>Space :-</b> 8 X 8 sq. ft</h5>
+                        <br>
+    		    	    <h6><b>Facilities Provided :</b></h6>
+    		    	    <ol>
+
+    					    <li>Plug Point : 1 No.</li>
+                            <li>Table : 2 No.</li>
+                            <li>Chair : 3 No.</li>
+                            <li>Water Facility</li>
+                        </ol>
+                        <h6><b>Brand Yourself Amongst</b></h6>
+                        <ul>
+
+                            <li>Best Minds</li>
+                            <li>Technological Enthusiasts</li>
+                            <li>Upcoming Engineers</li>
+                            <li>Young Entrepreneurs</li>
+                            <li>Researchers</li>
+                        </ul>
+
+    		    	</div>
+    		    	<div id="bizmantra5">
+                        <h6>E-Cell, VNIT Nagpur presents <b>Startup-Expo</b></h6>
+                        <h6>an event to showcase your entrepreneurial and business bravura, under the aegis of Consortium'18 - The Annual E-summit of VNIT.</h6>
+                        <br>
+                        <h6><b>Opportunities with us :-</b></h6>
+    			     	<ol>
+    			     	    <li>A chance to present your ideas afront venerable angel investors, technocrats,mentors and business houses who will be a part of Consortium'18.</li>
+    			     	    <li>IIM Nagpur in the premises of VNIT- a blend of management and technical avenues to present your ideas and expand your enterprises.</li>
+    			     	    <li>Seek authentic feedbacks and reviews from the right people.</li>
+    			     	    <li>A productive environment to propagate the lustre of your ideas and grandly publicise.</li>
+    			     	    <li>Behold an opportunity to brand your ideas in one of Central India's largest E-summit.</li>
+    			     	    <li>Get acknowledged with a certificate from E-Cell VNIT according your distinct presence in Startup Expo.</li>
+    			     	</ol>
+    			     	<br>
+    			     	<a href="#newform">Register</a>
+    			     	<p></p>
+
+    				</div>
+
+    		    </div>
+		  </div>
+        </div>
+        <!-- End Features -->
+
+        <!-- Coordinators -->
+        <div class="container js__parallax-window">
+            <div class="" id="cordinator">
+                <h2 class="g-font-size-40--xs g-font-size-50--sm g-font-size-40--md g-color--white">Contact Us</h2>
+                <ol class="row">
+                    <li class="col-md-6 g-color--white">
+                        <h6 class="g-color--white g-font-size-20--md">Utkarsh Gupta</h6>
+                        <p class=" g-color--white">vnitutkrshgpta@gmail.com</p>
+                        <p class=" g-color--white">+91 80058-81457</p>
+                    </li>
+                    <li class="col-md-6 g-color--white">
+                        <h6 class="g-color--white g-font-size-20--md">Shubham Jadhav</h6>
+                        <p class=" g-color--white">shubhamjadhav690@gmail.com</p>
+                        <p class=" g-color--white">+91 80076-57591</p>
+                    </li>
+
+                </ol>
+            </div>
+            <div id="newform">
+                <form class="g-bg-color--dark-light g-padding-x-50--xs g-padding-y-80--xs g-radius--4">
+                    <div class="g-text-center--xs">
+                        <a href="https://expo.ecellvnit.org/register/" target="_blank" class="text-uppercase btn-block s-btn s-btn--md s-btn--white-bg g-radius--50 g-padding-x-50--xs g-margin-b-20--xs">Register</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <!-- End Coordinator -->
+
+        <!--========== END PAGE CONTENT ==========-->
+
+        <!--========== FOOTER ==========-->
+        <?php include("footer.php");?>
+        <!--========== END FOOTER ==========-->
+
+        <!-- Back To Top -->
+        <a href="javascript:void(0);" class="s-back-to-top js__back-to-top"></a>
+
+        <!--========== JAVASCRIPTS (Load javascripts at bottom, this will reduce page load time) ==========-->
+        <!-- Vendor -->
+        <script type="text/javascript" src="vendor/jquery.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.migrate.min.js"></script>
+        <script type="text/javascript" src="vendor/bootstrap/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.smooth-scroll.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.back-to-top.min.js"></script>
+        <script type="text/javascript" src="vendor/scrollbar/jquery.scrollbar.min.js"></script>
+        <script type="text/javascript" src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+        <script type="text/javascript" src="vendor/swiper/swiper.jquery.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.parallax.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery.wow.min.js"></script>
+        <script src="js/cards.js"></script>
+		<script src="js/materialize.js"></script>
+
+        <!-- General Components and Settings -->
+        <script type="text/javascript" src="js/global.min.js"></script>
+        <script type="text/javascript" src="js/components/header-sticky.min.js"></script>
+        <script type="text/javascript" src="js/components/scrollbar.min.js"></script>
+        <script type="text/javascript" src="js/components/magnific-popup.min.js"></script>
+        <script type="text/javascript" src="js/components/swiper.min.js"></script>
+        <script type="text/javascript" src="js/components/parallax.min.js"></script>
+        <script type="text/javascript" src="js/components/wow.min.js"></script>
+        <!--========== END JAVASCRIPTS ==========-->
+
+    </body>
+    <!-- End Body -->
+</html>
