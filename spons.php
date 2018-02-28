37c7d98769afda2ab260198d537af895ead26689
Vipul Wairagade <sonu.wairagade@gmail.com>
2018-02-28 15:40:46 +0530

    Update spons.php

--- spons.php
+++ spons.php
@@ -7,7 +7,7 @@
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Consortium'18 | E-CELL, VNIT</title>
+        <title>Sponsors | E-CELL, VNIT</title>
         <meta name="keywords" content="E-cell VNIT entreprenuship cell consortium 18" />
         <meta name="description" content="Consortium'18 E-CELL VNIT annual entrepreneurship summit">
         <meta name="author" content="E-cell">
@@ -32,8 +32,19 @@
         <link href="css/theme/red.css" rel="stylesheet" type="text/css"/>
 
         <!-- Favicon -->
-        <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-        <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+        <link rel="shortcut icon" href="img/icon.png" type="image/x-icon">
+        <link rel="apple-touch-icon" href="img/icon.png" style="width:100%">
+
+        <!--W3-->
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<style>
+			img {margin-bottom: -7px;}
+			.w3-row-padding img {margin-bottom: 12px;}
+		</style>
+
     </head>
     <!-- End Head -->
 
@@ -55,142 +66,61 @@
         <!--========== PAGE CONTENT ==========-->
 
 <!-- sponsers-->
-					<article id="spons" class="container" style="background: #ddf 50% 0 no-repeat fixed;">
-						<div class="page-wrapper">
-							<div class="content-padding clearfix">
-							  <div class="row" style="text-align:center; padding-top:20px;" >
 
-							    <h2>Flagship'17</h2>
-							  </div>
-								<div class="row">
-									<div class="col 4">
-									  <img src="/img/SponS/ola.jpg">
-									</div>
-									<div class="col 4">
-									  <img src="/img/SponS/makxenia.png">
-									</div>
 
+							  <!-- !PAGE CONTENT! -->
+								<div class="w3-content" style="max-width:1500px">
+
+								<!-- Photo Grid -->
+								<div class="w3-row-padding" id="myGrid" style="margin-bottom:0px">
+								  <div class="w3-quarter">
+								    <img src="img/SponS/final/2.png" style="width:100%">
+									<img src="img/SponS/final/8.png" style="width:100%">
+									<img src="img/SponS/final/9.png" style="width:100%">
+									<img src="img/SponS/final/.png" style="width:100%">
+									<img src="img/SponS/final/.png" style="width:100%">
+									<img src="img/SponS/final/.png" style="width:100%">
+									<img src="img/SponS/final/.png" style="width:100%">
+								  </div>
+
+								  <div class="w3-quarter">
+									<img src="img/SponS/final/6.png" style="width:100%">
+									<img src="img/SponS/final/4.png" style="width:100%">
+									<img src="img/SponS/final/10.png" style="width:100%">
+									<img src="img/SponS/final/.png" style="width:100%">
+									<img src="img/SponS/final/.png" style="width:100%">
+									<img src="img/SponS/final/.png" style="width:100%">
+									<img src="img/SponS/final/.png" style="width:100%">
+									<img src="img/SponS/final/.png" style="width:100%">
+								  </div>
+
+								  <div class="w3-quarter">
+									<img src="img/SponS/final/5.png" style="width:100%">
+									<img src="img/SponS/final/1.png" style="width:100%">
+									<img src="img/SponS/final/11.png" style="width:100%">
+									<img src="img/SponS/final/.png" style="width:100%">
+									<img src="img/SponS/final/.png" style="width:100%">
+									<img src="img/SponS/final/.png" style="width:100%">
+									<img src="img/SponS/final/.png" style="width:100%">
+									<img src="img/SponS/final/.png" style="width:100%">
+								  </div>
+
+								  <div class="w3-quarter">
+									<img src="img/SponS/final/7.png" style="width:100%">
+									<img src="img/SponS/final/3.png" style="width:100%">
+									<img src="img/SponS/final/.png" style="width:100%">
+									<img src="img/SponS/final/.png" style="width:100%">
+									<img src="img/SponS/final/.png" style="width:100%">
+									<img src="img/SponS/final/.png" style="width:100%">
+									<img src="img/SponS/final/.png" style="width:100%">
+									<img src="img/SponS/final/.png" style="width:100%">
+									<img src="img/SponS/final/.png" style="width:100%">
+								  </div>
+								 </div>
+
+								<!-- End Page Content -->
 								</div>
 
-							</div>
-
-							<div class="content-padding clearfix">
-							  <div class="row"  style="text-align:center;">
-							    <h2>Our Previous Sponsor</h2>
-							  </div>
-							<div class="row">
-							<div class="col m3 s6">
-							  <img src="/img/SponS/Coca-Cola.png">
-							</div>
-							<div class="col m3 s6">
-							  <img src="/img/SponS/bsnl.jpg">
-							</div>
-							<div class="col m3 s6">
-							  <img src="/img/SponS/ccd.jpg">
-							</div>
-							<div class="col m3 s6">
-							  <img src="/img/SponS/central.jpg">
-							</div>
-							</div>
-							<div class="row">
-
-							<div class="col m3 s6">
-							  <img src="/img/SponS/dhl.png">
-							</div>
-							<div class="col m3 s6">
-							  <img src="/img/SponS/gate.png">
-							</div>
-							<div class="col m3 s6">
-							  <img src="/img/SponS/hrmentors.png">
-							</div>
-							<div class="col m3 s6">
-							  <img src="/img/SponS/aeolus.jpg">
-							</div>
-							</div>
-							<div class="row">
-
-							<div class="col m3 s6">
-							  <img src="/img/SponS/arun.jpg">
-							</div>
-							<div class="col m3 s6">
-							  <img src="/img/SponS/astral.png">
-							</div>
-							<div class="col m3 s6">
-							  <img src="/img/SponS/_kickopick.png">
-							</div>
-							<div class="col m3 s6">
-							  <img src="/img/SponS/chate.jpg">
-							</div>
-							</div>
-							<div class="row">
-
-							<div class="col m3 s6">
-							  <img src="/img/SponS/frankie.png">
-							</div>
-							<div class="col m3 s6">
-							  <img src="/img/SponS/iball.png">
-							</div>
-							<div class="col m3 s6">
-							  <img src="/img/SponS/itc.png">
-							</div>
-							<div class="col m3 s6">
-							  <img src="/img/SponS/johnson.jpg">
-							</div>
-							</div>
-							<div class="row">
-
-							<div class="col m3 s6">
-							  <img src="/img/SponS/lic.png">
-							</div>
-							<div class="col m3 s6">
-							  <img src="/img/SponS/mastersoft.png">
-							</div>
-							<div class="col m3 s6">
-							  <img src="/img/SponS/national.png">
-							</div>
-							<div class="col m3 s6">
-							  <img src="/img/SponS/newindia.png">
-							</div>
-							</div>
-							<div class="row">
-
-							<div class="col m3 s6">
-							  <img src="/img/SponS/oyo.png">
-							</div>
-							<div class="col m3 s6">
-							  <img src="/img/SponS/radiocity.jpg">
-							</div>
-							<div class="col m3 s6">
-							  <img src="/img/SponS/sidbi.jpg">
-							</div>
-							<div class="col m3 s6">
-							  <img src="/img/SponS/tie.JPG">
-							</div>
-							</div>
-							<div class="row">
-
-							<div class="col m3 s6">
-							  <img src="/img/SponS/time.png">
-							</div>
-							<div class="col m3 s6">
-							  <img src="/img/SponS/uber.jpg">
-							</div>
-							<div class="col m3 s6">
-							  <img src="/img/SponS/vidarbh.png">
-							</div>
-							<div class="col m3 s6">
-							<img src="/img/SponS/zebronics.png">
-							</div>
-							</div>
-							<div class="row row-centered">
-							<div class="col-md-3 col-centered">
-							  <img src="/img/SponS/vidyalankar.png">
-							</div>
-							</div>
-
-							</div>
-						</div>
-					</article>
         <!--========== END PAGE CONTENT ==========-->
 
         <!--========== FOOTER ==========-->
